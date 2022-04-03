44c885422d4c452c59d2228d9ccdee7a4a2eb5b3
Дмитрий Минаков <m1nak0vd@yandex.ru>
2022-04-03 14:08:31 +0300

    Добавьте файлы проекта.

---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Груша",
+                Category = Categories.Food,
+                Price = 100,
+                Image = "Data/ab.jpg"
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Блендер",
+                Category = Categories.Appliances,
+                Price = 2000,
+                Image = "Data/s-l.jpg"
+            });
+            lstbox.ItemsSource = products;
+        }
+    }
+}
